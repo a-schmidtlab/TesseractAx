fc3006b714b31c513388f7034f3aa248d8da6442
AxScLab <axel@schmidtlab.net>
2025-02-10 12:55:43 +0100

    Initial Commit

---
+++ SofaRotator/SofaRotatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofaRotatorApp.swift
+//  SofaRotator
+//
+//  Created by Axel Schmidt on 10.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SofaRotatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SofaRotatorTests/SofaRotatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SofaRotatorTests.swift
+//  SofaRotatorTests
+//
+//  Created by Axel Schmidt on 10.02.25.
+//
+
+import Testing
+@testable import SofaRotator
+
+struct SofaRotatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
